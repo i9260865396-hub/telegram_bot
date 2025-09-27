62725894bc9034c22a30dcee8f5e3cbf26a2643f
i9260865396-hub <i9260865396@gmail.com>
2025-09-27 19:16:40 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from config.settings import settings
 from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
 from database.base import async_session
 from database.models import Order, Service, Admin
 
